d3124d1b56d778966fb12a23a7e935b111f194af
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-11 16:16:18 -0400

    Write retrieve_id func to open file or generate random id

--- credit_for_mining_p/miner.py
+++ credit_for_mining_p/miner.py
@@ -2,6 +2,19 @@ import hashlib
 import requests
 
 import sys
+import os.path
+from uuid import uuid4
+
+# check for existence of my_id
+def retrieve_id():
+    if os.path.exists('my_id.txt'):
+        with open('my_id.txt') as f:
+            my_id = f.read()
+            f.close()
+            return my_id
+    else:
+        return str(uuid4()).replace('-', '')
+
 
 
 def proof_of_work(last_proof):
@@ -12,12 +25,12 @@ def proof_of_work(last_proof):
     - p is the previous proof, and p' is the new proof
     """
 
-    print("Searching for next proof")
+    print("\nSearching for next proof\n")
     proof = 0
     while valid_proof(last_proof, proof) is False:
         proof += 1
 
-    print("Proof found: " + str(proof))
+    print("\nProof found: " + str(proof))
     return proof
 
 
@@ -40,18 +53,19 @@ if __name__ == '__main__':
 
     coins_mined = 0
     # Run forever until interrupted
-    while True:
-        # Get the last proof from the server
-        r = requests.get(url=node + "/last_proof")
-        data = r.json()
-        new_proof = proof_of_work(data.get('proof'))
+    print(retrieve_id())
+    # while True:
+    #     # Get the last proof from the server
+    #     r = requests.get(url=node + "/last_proof")
+    #     data = r.json()
+    #     new_proof = proof_of_work(data.get('proof'))
 
-        post_data = {"proof": new_proof}
+    #     post_data = {"proof": new_proof}
 
-        r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
-        if data.get('message') == 'New Block Forged':
-            coins_mined += 1
-            print("Total coins mined: " + str(coins_mined))
-        else:
-            print(data.get('message'))
+    #     r = requests.post(url=node + "/mine", json=post_data)
+    #     data = r.json()
+    #     if data.get('message') == 'New Block Forged':
+    #         coins_mined += 1
+    #         print("Total coins mined: " + str(coins_mined))
+    #     else:
+    #         print(data.get('message'))
