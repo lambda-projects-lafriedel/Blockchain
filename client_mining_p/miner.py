0617b34eba350a5c20336b41654619ca43ed9111
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-10 20:55:15 -0400

    Proof of work and validate proof function work on client side

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -19,7 +19,35 @@ if __name__ == '__main__':
     # while True:
     # TODO: Get the last proof from the server and look for a new one
     proof = requests.get('http://localhost:5000/last_proof')
-    print(proof.text)
+    print("LAST PROOF", proof.json()['proof'])
+
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof:  Does hash(last_proof, proof) contain 6
+        leading zeroes?
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:6] == "000000"
+
+    def proof_of_work(last_proof):
+        """
+        Simple Proof of Work Algorithm
+        - Find a number p' such that hash(pp') contains 4 leading
+        zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        """
+        print(f'\nSearch for proof initialized.\n')
+
+        proof = 0
+        while valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        print(f'\nSearch for proof complete, proof is {proof}\n')
+        return proof
+
+    proof_of_work(proof.json()['proof'])
+    
     # TODO: When found, POST it to the server {"proof": new_proof}
     # TODO: If the server responds with 'New Block Forged'
     # add 1 to the number of coins mined and print it.  Otherwise,
